423e3217549d4f2178f5e6b61b6fab33340eb093
luiferna <luiferna@LES011096.corp.capgemini.com>
2023-10-24 17:45:49 +0200

    Probando nueva rama "Luis"

--- biblioteca-spring/src/main/java/com/biblioteca/start/BibliotecaSpringApplication.java
+++ biblioteca-spring/src/main/java/com/biblioteca/start/BibliotecaSpringApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BibliotecaSpringApplication {
-
+	
+	// rama de luis
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaSpringApplication.class, args);
 	}
