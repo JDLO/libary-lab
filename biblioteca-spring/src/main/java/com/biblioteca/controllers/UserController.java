d74e2f76f67fe41a4bcf8f6cf34843211af16519
Luis Fernandez Suarez <uo271405@uniovi.es>
2023-10-26 16:51:06 +0200

    Si falla el login, se muestra error al usuario

--- biblioteca-spring/src/main/java/com/biblioteca/controllers/UserController.java
+++ biblioteca-spring/src/main/java/com/biblioteca/controllers/UserController.java
@@ -1,12 +1,14 @@
 package com.biblioteca.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.biblioteca.entities.User;
@@ -31,6 +33,26 @@ public class UserController {
 	public String getLogin() {
 		return "user/login";
 	}
+	
+	/**
+	 * Método que maneja la solicitud GET "/failure-login" para mostrar la página de
+	 * inicio de sesión con un mensaje de error en caso de fallo de inicio de
+	 * sesión.
+	 * 
+	 * @param model El modelo utilizado para pasar atributos a la vista.
+	 * @return La vista "login" con un mensaje de error si el usuario no ha iniciado
+	 *         sesión, o una redirección a la ruta "/home" si el usuario ya tiene
+	 *         sesión iniciada.
+	 */
+	@GetMapping(value = "/failure-login")
+	public String handleLoginFailure(Model model) {
+		// Si el usuario ya tiene sesión iniciada, redirigir a la página principal
+		if (getActiveUser() != null) {
+			return "redirect:/home";
+		}
+		model.addAttribute("loginFailure", true);
+		return "user/login";
+	}
 
 	@GetMapping("/admin/add")
 	public String getAddAdmin(Model model) {
@@ -47,5 +69,14 @@ public class UserController {
 		}
 		userService.agregar(userValidated);
 		return "redirect:/home";
+	}
+	
+	/**
+	 * Devuelve el usuario con sesión iniciada en el sistema.
+	 * 
+	 * @return El usuario con la sesión iniciada.
+	 */
+	private User getActiveUser() {
+		return userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
 	}
 }
