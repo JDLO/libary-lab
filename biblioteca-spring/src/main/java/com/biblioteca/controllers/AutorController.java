eb86f771650f27b7c42cb12947c48e7822c03bad
Luis Fernandez Suarez <uo271405@uniovi.es>
2023-11-03 09:41:22 +0100

    Actualizado fallo e implementados mensajes de exito en autor/list

--- biblioteca-spring/src/main/java/com/biblioteca/controllers/AutorController.java
+++ biblioteca-spring/src/main/java/com/biblioteca/controllers/AutorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.biblioteca.entities.Autor;
 import com.biblioteca.services.AutorService;
@@ -27,8 +28,9 @@ public class AutorController {
 	}
 
 	@PostMapping("/autor/add")
-	public String saveAutor(@ModelAttribute("author") Autor autor) {
+	public String saveAutor(@ModelAttribute("author") Autor autor, RedirectAttributes redAttrs) {
 		autorService.agregar(autor);
+		redAttrs.addFlashAttribute("autorGuardado", true);
 		return "redirect:/autor/list";
 	}
 
